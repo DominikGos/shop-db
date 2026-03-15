a6268d1553c133952bc42e6ce03e0c994ae4d92e
DominikGos <dominikgos3@gmail.com>
2026-03-15 14:55:43 +0100

    Init

---
+++ backend/shop-db/src/main/java/com/db/shop/ShopDbApplication.java
@@ -0,0 +1,13 @@
+package com.db.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopDbApplication.class, args);
+	}
+
+}
